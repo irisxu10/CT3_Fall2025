b14df38d546be5bff2600ef6c7cd13fa935cfd5d
irisxu10 <xxu10@inside.artcenter.edu>
2025-09-26 16:57:53 -0700

    some changes

--- CT3_Doc/header.js
+++ CT3_Doc/header.js
@@ -1,9 +1,9 @@
 function getPathPrefix() {
   const path = window.location.pathname;
-  if (path.includes('/mdp/')) {
-    return '../'; // 说明当前页面在 mdp 子目录下
+  if (path.includes('/CT3_Docs/')) {
+    return ''; 
   } else {
-    return ''; // 说明当前页面在根目录
+    return '../'; 
   }
 }
 
@@ -13,16 +13,21 @@ document.querySelector('header').innerHTML = `
   <div class="content">
     <div class="logo">IRIS</div>
     <div class="menu-list">
-      <div class="menu-item color-2" onclick="toPage('${prefix}index.html')">HOME</div>
-      <div class="menu-item color-4" onclick="toPage('${prefix}about.html')">About Me</div>
-      <div class="menu-item color-6" onclick="toPage('${prefix}mdp.html')">MDP Works</div>
-      <div class="menu-item color-7" onclick="toPage('${prefix}writings.html')">Writings</div>
-      <div class="menu-item color-5" onclick="toPage('${prefix}projects.html')">Projects</div>
-      <div class="menu-item color-9" onclick="toPage('${prefix}artworks.html')">Artworks</div>
-    </div>
+      <div class="menu-item color-2" onclick="toPage('${prefix}ct3.html')">CT3_HOME</div>
   </div>
 `;
+
 
 function toPage(page) {
   window.location.href = page;
 }
+
+
+//    <div class="menu-list">
+    //   <div class="menu-item color-2" onclick="toPage('${prefix}index.html')">HOME</div>
+    //   <div class="menu-item color-4" onclick="toPage('${prefix}about.html')">About Me</div>
+    //   <div class="menu-item color-6" onclick="toPage('${prefix}mdp.html')">MDP Works</div>
+    //   <div class="menu-item color-7" onclick="toPage('${prefix}writings.html')">Writings</div>
+    //   <div class="menu-item color-5" onclick="toPage('${prefix}projects.html')">Projects</div>
+    //   <div class="menu-item color-9" onclick="toPage('${prefix}artworks.html')">Artworks</div>
+    // </div>
